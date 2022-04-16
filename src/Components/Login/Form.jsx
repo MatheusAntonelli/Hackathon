59f2a5cbb816a7b0635f2abdf5cba1c9220026a3
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2022-04-15 22:05:18 -0300

    Update an unused var

--- src/Components/Login/Form.jsx
+++ src/Components/Login/Form.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-const PESSOA = ['Fisica', 'Juridica'];
 export const Form = ({
     type,
     fullName,
